601e46f113ed52d275135a5dd25c79f823bac1d5
Petr Bednář <p.petr.bednar@gmail.com>
2021-12-09 01:54:26 +0100

    Implemented search funcionality

--- src/io/github/ppetrbednar/tmdb/api/Tmdb.java
+++ src/io/github/ppetrbednar/tmdb/api/Tmdb.java
@@ -9,6 +9,8 @@ import java.net.URL;
 import org.apache.commons.io.FileUtils;
 import io.github.ppetrbednar.tmdb.api.exception.ApiException;
 import io.github.ppetrbednar.tmdb.wrappers.movies.MovieMeta;
+import io.github.ppetrbednar.tmdb.wrappers.results.MovieResults;
+import io.github.ppetrbednar.tmdb.wrappers.results.SeriesResults;
 import io.github.ppetrbednar.tmdb.wrappers.series.EpisodeMeta;
 import io.github.ppetrbednar.tmdb.wrappers.series.SeasonMeta;
 import io.github.ppetrbednar.tmdb.wrappers.series.SeriesMeta;
@@ -19,8 +21,10 @@ import io.github.ppetrbednar.tmdb.wrappers.series.SeriesMeta;
  */
 public class Tmdb {
 
-    private static final String TV = "https://api.themoviedb.org/3/tv/";
+    private static final String SERIES = "https://api.themoviedb.org/3/tv/";
+    private static final String SEARCH_SERIES = "https://api.themoviedb.org/3/search/tv";
     private static final String MOVIE = "https://api.themoviedb.org/3/movie/";
+    private static final String SEARCH_MOVIE = "https://api.themoviedb.org/3/search/movie";
     private static final String IMAGE = "https://image.tmdb.org/t/p/";
 
     private final String API_KEY;
@@ -33,9 +37,31 @@ public class Tmdb {
         this.SUFFIX = "?api_key=" + apiKey + "&language=" + language.getLanguage();
     }
 
+    public SeriesResults searchForSeries(String title) throws ApiException {
+        try {
+            String data = ApiCall.call(SEARCH_SERIES + SUFFIX + "&query=" + title);
+            JsonObject json = (JsonObject) Jsoner.deserialize(data);
+            SeriesResults meta = new SeriesResults(json);
+            return meta;
+        } catch (JsonException ex) {
+            return null;
+        }
+    }
+
+    public MovieResults searchForMovie(String title) throws ApiException {
+        try {
+            String data = ApiCall.call(SEARCH_MOVIE + SUFFIX + "&query=" + title);
+            JsonObject json = (JsonObject) Jsoner.deserialize(data);
+            MovieResults meta = new MovieResults(json);
+            return meta;
+        } catch (JsonException ex) {
+            return null;
+        }
+    }
+
     public SeriesMeta getSeriesMeta(int seriesId) throws ApiException {
         try {
-            String data = ApiCall.call(TV + seriesId + SUFFIX);
+            String data = ApiCall.call(SERIES + seriesId + SUFFIX);
             JsonObject json = (JsonObject) Jsoner.deserialize(data);
             SeriesMeta meta = new SeriesMeta(json);
             return meta;
@@ -46,7 +72,7 @@ public class Tmdb {
 
     public SeasonMeta getSeasonMeta(int seriesId, int seasonNumber) throws ApiException {
         try {
-            String data = ApiCall.call(TV + seriesId + "/season/" + seasonNumber + SUFFIX);
+            String data = ApiCall.call(SERIES + seriesId + "/season/" + seasonNumber + SUFFIX);
             JsonObject json = (JsonObject) Jsoner.deserialize(data);
             SeasonMeta meta = new SeasonMeta(json);
             return meta;
@@ -57,7 +83,7 @@ public class Tmdb {
 
     public EpisodeMeta getEpisodeMeta(int seriesId, int seasonNumber, int episodeNumber) throws ApiException {
         try {
-            String data = ApiCall.call(TV + seriesId + "/season/" + seasonNumber + "/episode/" + episodeNumber + SUFFIX);
+            String data = ApiCall.call(SERIES + seriesId + "/season/" + seasonNumber + "/episode/" + episodeNumber + SUFFIX);
             JsonObject json = (JsonObject) Jsoner.deserialize(data);
             EpisodeMeta meta = new EpisodeMeta(json);
             return meta;
